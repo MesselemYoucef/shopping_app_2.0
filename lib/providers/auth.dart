5b146da978bf6556c7b6606c91224807511b6de9
Youcef <messelemy@gmail.com>
2022-10-03 15:07:11 +0400

    brought the token key from the project

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,14 +1,14 @@
-
-
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class Auth with ChangeNotifier{
+class Auth with ChangeNotifier {
   String? _token;
   DateTime? _expiryDate;
   String? _userId;
 
-  Future<void> signup(String email, String password) async{
-    
+  Future<void> signup(String email, String password) async {
+    var url = Uri.https(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCH4Z2ZLDia-6ETltU-mOXECPBOCxSQv8c");
+    http.post(url);
   }
 }
