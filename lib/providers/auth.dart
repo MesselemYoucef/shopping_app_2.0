a2e5e0ac2bbfcf0247f86ef6b85b8f57aa14ef3b
Youcef <messelemy@gmail.com>
2022-10-06 13:08:18 +0400

    fixed the signin method

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,21 +7,10 @@ class Auth with ChangeNotifier {
   DateTime? _expiryDate;
   String? _userId;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String UrlSegment) async {
     final url = Uri.parse(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCH4Z2ZLDia-6ETltU-mOXECPBOCxSQv8c");
-    final response = await http.post(url,
-        body: json.encode({
-          "email": email,
-          "password": password,
-          "returnSecureToken": true,
-        }));
-    // print(json.decode(response.body));
-  }
-
-  Future<void> singin(String email, String password) async {
-    final url = Uri.parse(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCH4Z2ZLDia-6ETltU-mOXECPBOCxSQv8c");
+        "https://identitytoolkit.googleapis.com/v1/accounts:${UrlSegment}?key=AIzaSyCH4Z2ZLDia-6ETltU-mOXECPBOCxSQv8c");
     final response = await http.post(url,
         body: json.encode({
           "email": email,
@@ -30,5 +19,13 @@ class Auth with ChangeNotifier {
         }));
 
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> singin(String email, String password) async {
+    _authenticate(email, password, "signInWithPassword");
   }
 }
